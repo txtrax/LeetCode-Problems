6c45f3ac3adbdf906cbb9d46587296346a2050a1
Theresa Tran <theresa.tran216@gmail.com>
2022-10-01 23:43:08 -0700

    Time: 159 ms (11.38%), Space: 46.3 MB (40.07%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    var result = [];
+    nums.forEach(num => {
+        result.push(nums[num])
+    })
+    return result;
+};
