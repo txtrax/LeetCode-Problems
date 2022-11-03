d95925d477b3901698a5b65f64bf90a10276fb4b
Theresa Tran <theresa.tran216@gmail.com>
2022-11-02 20:38:21 -0700

    Time: 1876 ms (11.05%), Space: 48.2 MB (96.39%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -7,16 +7,9 @@ var containsDuplicate = function(nums) {
     for (var i = 0; i < nums.length; i++) {
         if (nums.indexOf(nums[i], i + 1) !== -1) {
             return true;
-        }
-        
+        } 
     }
     
     return false;
-//     var isDuplicate = false;
-//     nums.forEach((num, i) => {
-//         if (nums.indexOf(num, i + 1) !== -1) {
-//             isDuplicate = true;
-//         }
-//     })
-//     return isDuplicate;
+
 };
